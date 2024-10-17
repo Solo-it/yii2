a1eea0516dc368e99de604fa4ad677b9dad3a628
Aleksey Solovev <sas@solo-it.ru>
2024-10-17 20:44:46 +0400

    Подключение к БД работает

--- www/index.php
+++ www/index.php
@@ -1,3 +1,15 @@
 <?php
-echo "Testing a PHP Apache Docker Container";
+$host = 'mysql-db';
+$user = 'db_user';
+$pass = 'ukHLIUFNYiulgyi4h';
+$db = 'test_database';
 
+$conn = new mysqli($host, $user, $pass, $db);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected to MySQL successfully";
+
+$conn->close();
